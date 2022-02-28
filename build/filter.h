2f2c17c2a15d89d1049b7f627a0248aead62b1df
Markus Sihler <uusqf@student.kit.edu>
2022-02-28 16:52:09 +0100

    added new box scene

--- build/filter.h
+++ build/filter.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "filter/box.h"
 #include "filter/blackmanharris.h"
-#define filter_splat filter_blackmanharris_splat
-#define filter_splat4 filter_blackmanharris_splat4
-#define filter_print_info filter_blackmanharris_print_info
+#define filter_splat filter_box_splat
+#define filter_splat4 filter_box_splat4
+#define filter_print_info filter_box_print_info
